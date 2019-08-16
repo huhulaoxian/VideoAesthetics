d67eaa0d561f2196a24995aef908f0135173ff4e
yonggeol93 <yonggeol93@gmail.com>
2019-08-16 09:59:13 +0900

    LOD 추가

--- Handcraft/MachajdikFeatures.py
+++ Handcraft/MachajdikFeatures.py
@@ -42,3 +42,29 @@ def dynamics(img):
         abs_degree_dynamics = np.mean(np.abs(slant_dict['degree']))
 
     return [len_statics, degree_statics, abs_degree_statics, len_dynamics, degree_dynamics, abs_degree_dynamics]
+
+def LevelOfDetail(img):
+   # binaray image로 변환
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+
+    #Morphology의 opening, closing을 통해서 노이즈나 Hole제거
+    kernel = np.ones((3,3),np.uint8)
+    opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel,iterations=2)
+
+    # dilate를 통해서 확실한 Backgroud
+    sure_bg = cv2.dilate(opening,kernel,iterations=3)
+
+    #distance transform을 적용하면 중심으로 부터 Skeleton Image를 얻을 수 있음.
+    # 즉, 중심으로 부터 점점 옅어져 가는 영상.
+    # 그 결과에 thresh를 이용하여 확실한 FG를 파악
+    dist_transform = cv2.distanceTransform(opening,cv2.DIST_L2,5)
+    ret, sure_fg = cv2.threshold(dist_transform,0.5*dist_transform.max(),255,0)
+    sure_fg = np.uint8(sure_fg)
+
+    # Background에서 Foregrand를 제외한 영역을 Unknow영역으로 파악
+    unknown = cv2.subtract(sure_bg, sure_fg)
+
+    ret, markers = cv2.connectedComponents(sure_fg)
+
+    return ret
